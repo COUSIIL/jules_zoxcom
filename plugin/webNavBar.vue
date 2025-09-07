1a28b437b59c3e8051127e9702ade2fc5038178f
Aliouane Coussila <116021290+COUSIIL@users.noreply.github.com>
2025-09-07 21:23:28 +0100

    Revert "Fix dark mode, sidebar, and navbar layout"

--- plugin/webNavBar.vue
+++ plugin/webNavBar.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <Nav :user="user" :isMounted="isMounted" :isDark="isDark" :isDeasy="isDeasy" :isAuthenticated="isAuthenticated" :logoDark="logoDark" :logoWhite="logoWhite" :varqWhite="varqWhite" :varqDark="varqDark" :isVisible="isVisible" @darkMode="toggleDarkMode" @sideBar="viewMenu" @handleLogout="handleLogout"/>
+  <Nav :user="user" :isMounted="isMounted" :isDark="isDark" :isDeasy="isDeasy" :isAuthenticated="isAuthenticated" :logoDark="logoDark" :logoWhite="logoWhite" :varqWhite="varqWhite" :varqDark="varqDark" :isVisible="isVisible" @darkMode="toggleDarkMode" @sideBar="viewMenu" @viewMenu="toggleDarkMode" @handleLogout="handleLogout"/>
 
 
   <SideBar v-if="isAuthenticated" @viewMenu="viewMenu" @handleLogout="handleLogout" :isVisible="isVisible" @close="isVisible = false"/>
@@ -23,7 +23,7 @@ const { t } = useLang();
 
 const isMounted = ref(false);
 const isVisible = ref(false);
-const isDark = ref(false);
+const isDark = ref();
 const user = ref(t('connexion'));
 const isAuthenticated = ref(false);
 const isDeasy = ref(true);
@@ -46,20 +46,21 @@ onMounted(() => {
       user.value = authData.username;
       isAuthenticated.value = true;
     }
+    
+    
   }
+    // Vérifie si le thème est enregistré dans le localStorage
+    if (localStorage.getItem('darkMode')) {
+      isDark.value = JSON.parse(localStorage.getItem('darkMode'));
+    } else {
+      // Si rien dans le localStorage, détecte le thème de l'appareil
+      isDark.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
 
-  // Vérifie si le thème est enregistré dans le localStorage
-  if (localStorage.getItem('darkMode')) {
-    isDark.value = JSON.parse(localStorage.getItem('darkMode'));
-  } else {
-    // Si rien dans le localStorage, détecte le thème de l'appareil
-    isDark.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  }
+    // Applique la classe en fonction du thème détecté
+    document.documentElement.classList.toggle('dark', isDark.value);
 
-  // Applique la classe en fonction du thème détecté
-  document.documentElement.classList.toggle('dark', isDark.value);
-
-  isMounted.value = true;
+    isMounted.value = true;
 });
 
 const viewMenu = () => {
@@ -70,7 +71,7 @@ const viewMenu = () => {
 const toggleDarkMode = () => {
   isDark.value = !isDark.value;
   document.documentElement.classList.toggle("dark", isDark.value);
-  localStorage.setItem('darkMode', JSON.stringify(isDark.value));
+  localStorage.setItem('darkMode', isDark.value);
 };
 
 
