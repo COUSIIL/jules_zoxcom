9b8e9f997e30caca639c3b0a270015ff3ff444d5
Aliouane Coussila <116021290+COUSIIL@users.noreply.github.com>
2026-01-30 18:36:34 +0100

    Revert "Fix unique code assignment logic in order update"

--- backend/sql/update/order.php
+++ backend/sql/update/order.php
@@ -247,12 +247,6 @@ if ($value === 'confirmed') {
                     $types = "i";
                     $params = [$prodId];
 
-                    if ($modId > 0) {
-                        $sqlFind .= " AND model_id = ?";
-                        $types .= "i";
-                        $params[] = $modId;
-                    }
-
                     if ($detailId > 0) {
                         $sqlFind .= " AND detail_id = ?";
                         $types .= "i";
@@ -272,7 +266,6 @@ if ($value === 'confirmed') {
                     $foundIds = [];
                     while ($row = $resFind->fetch_assoc()) {
                          $foundIds[] = $row['id'];
-                         $assignedCodes[] = $row;
                     }
                     $stmtFind->close();
 
